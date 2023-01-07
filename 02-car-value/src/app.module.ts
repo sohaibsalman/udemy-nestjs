46e7767303d4b5b43ca287f8b7be0152969a68e7
sohaibsalman <sohaibsalman10@gmail.com>
2023-01-07 22:26:55 +0500

    Init typeorm | Create user and report entites

--- 02-car-value/src/app.module.ts
+++ 02-car-value/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,9 +7,20 @@ import { ReportsModule } from './reports/reports.module';
 import { UsersModule } from './users/users.module';
 import { ReportsController } from './reports/reports.controller';
 import { UsersController } from './users/users.controller';
+import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
 
 @Module({
-  imports: [ReportsModule, UsersModule],
+  imports: [
+    ReportsModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User, Report],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, ReportsController, UsersController],
   providers: [AppService],
 })
