59982dab45c87319bdcfb14bd8ac682e0348ee13
sohaibsalman <sohaibsalman10@gmail.com>
2023-01-21 23:40:29 +0500

    Manage app configuration

--- 02-car-value/src/app.module.ts
+++ 02-car-value/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { ReportsModule } from './reports/reports.module';
 import { UsersModule } from './users/users.module';
@@ -10,11 +11,20 @@ import { Report } from './reports/report.entity';
   imports: [
     ReportsModule,
     UsersModule,
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [User, Report],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>("DB_NAME"),
+          entities: [User, Report],
+          synchronize: true,
+        };
+      },
     }),
   ],
   controllers: [],
