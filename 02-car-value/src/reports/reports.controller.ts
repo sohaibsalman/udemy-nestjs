493def98d6ed7ff8661f4e2181a10c5f67a9b33f
sohaibsalman <sohaibsalman10@gmail.com>
2023-01-22 13:39:12 +0500

    Add custom decorator for authorization

--- 02-car-value/src/reports/reports.controller.ts
+++ 02-car-value/src/reports/reports.controller.ts
@@ -2,12 +2,14 @@ import { Controller, Post, Body } from '@nestjs/common';
 
 import { ReportsService } from './reports.service';
 import { CreateReportDto } from './dtos/create-report.dto';
+import { UseAuth } from 'src/decorators/use-auth.decorator';
 
 @Controller('reports')
 export class ReportsController {
     constructor(private reportsService: ReportsService) { }
 
     @Post()
+    @UseAuth()
     createReport(@Body() reportDto: CreateReportDto) {
         return this.reportsService.create(reportDto);
     }
