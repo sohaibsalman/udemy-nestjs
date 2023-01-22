89450ad176c4f83208cbc31a710d584c67ffc198
sohaibsalman <sohaibsalman10@gmail.com>
2023-01-22 13:54:06 +0500

    Add user id property to reports response

--- 02-car-value/src/reports/reports.controller.ts
+++ 02-car-value/src/reports/reports.controller.ts
@@ -5,6 +5,8 @@ import { CreateReportDto } from './dtos/create-report.dto';
 import { UseAuth } from '../decorators/use-auth.decorator';
 import { CurrentUser } from '../users/decorators/current-user.decorator';
 import { User } from 'src/users/user.entity';
+import { ReportDto } from './dtos/report.dto';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
 
 @Controller('reports')
 export class ReportsController {
@@ -12,6 +14,7 @@ export class ReportsController {
 
     @Post()
     @UseAuth()
+    @Serialize(ReportDto)
     createReport(@CurrentUser() user: User, @Body() reportDto: CreateReportDto) {
         return this.reportsService.create(reportDto, user);
     }
